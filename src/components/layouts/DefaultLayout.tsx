342cebe85aeb3109ac053bbf20c0129c14f2ec7c
Tendani Netshitenzhe <tbnetshitenzhe@gmail.com>
2026-07-19 18:55:15 +0200

    renamed to default layout

---
+++ src/components/layouts/DefaultLayout.tsx
@@ -0,0 +1,21 @@
+import { useAuthStore } from "@/stores/authStore";
+import { Navigate, Outlet } from "react-router-dom";
+import { Center, Loader } from "@mantine/core";
+
+export default function DefaultLayout() {
+  const { profile, loading } = useAuthStore();
+
+  if (loading) {
+    return (
+      <Center h="100%">
+        <Loader size="xl" type="bars" />
+      </Center>
+    );
+  }
+
+  if (!profile) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <Outlet />;
+}
